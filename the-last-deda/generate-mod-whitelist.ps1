17a0a133c8c7a3cb603e3bfb816a7eda951b9397
Kawodeda <chuprikovdaniil0@gmail.com>
2025-10-15 22:32:00 +0700

    generate whitelist verbose logging

--- the-last-deda/generate-mod-whitelist.ps1
+++ the-last-deda/generate-mod-whitelist.ps1
@@ -6,13 +6,20 @@ Copy-Item -Path "./dynamic-mods-list.txt" -Destination $outPath
 ./extract-mod-id.ps1 -BaseLocation ".\src\shared\mods" -ResultPath $outPath -AppendResult
 ./extract-mod-id.ps1 -BaseLocation ".\src\server\mods" -ResultPath $outPath -AppendResult
 
+Write-Host "Extracted."
+
 $whitelistContent = Get-Content -Path $outPath -Raw
+Write-Host "Got whitelist"
 $whitelistItems = $whitelistContent.Trim().Trim(',') -split "," | ForEach-Object { $_.Trim().Trim('\"') }
+Write-Host "Parsed items"
 
 $configPath = "./src/server/config/mod_whitelist-config.json"
 $configContent = Get-Content -Path $configPath -Raw
+Write-Host "Got config file"
 $jsonObject = $configContent | ConvertFrom-Json
+Write-Host "Got config JSON"
 
 $jsonObject.CLIENT_MOD_WHITELIST = $whitelistItems
-
+Write-Host "Set whitelist"
 $jsonObject | ConvertTo-Json -Depth 3 | Out-File $configPath -Encoding UTF8
+Write-Host "Wrote output"
